bcf83928dd60f3995fcb3bf0fe4f7c30c80f5b61
ChillerDrgon <ChillerDragon@gmail.com>
2022-04-22 10:42:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# client-password-hack
+
+Some ugly hackish plugin to reload the autoexec.cfg once after launch
+This loads ``password`` calls after the client is running.
+
+This then fixes password being not send due to ddnet client being protective.
+
---
+++ side_runner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -f lib/lib.sh ]
+then
+        echo "Error: lib/lib.sh not found!"
+        echo "make sure you are in the root of the server repo"
+        exit 1
+fi
+
+source lib/lib.sh
+
+sleep 2
+
+log "[password-hack] reloading config to populate password"
+
+./lib/fifo.sh exec autoexec.cfg
+
